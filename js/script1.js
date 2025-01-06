4e6022b51d9bd98691e87fea4e70554793b765bc
TaSamaya2025 <SvetlanaVorobueva@yandex.com>
2025-01-06 04:24:28 +0300

    Initial commit

---
+++ js/script1.js
@@ -0,0 +1,168 @@
+/*Скрипт для бургер-меню*/
+var menuToggle = document.getElementById('menu-toggle');
+var navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', function() {
+  navLinks.classList.toggle('active');
+});
+
+var menuToggle = document.getElementById('menu-toggle');
+menuToggle.addEventListener('click', function() {
+  menuToggle.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        // Получаем бургер-меню
+        const burgerMenu = document.querySelector('.nav-links a');
+        // Удаляем активный класс, если нужно закрыть меню
+        burgerMenu.classList.remove('active');
+    });
+});
+
+// Функция для открытия модального окна
+var modal = document.getElementById('modal');
+var buttons = document.querySelectorAll('.price-button');
+var selectedPlan = document.getElementById('selected-plan');
+var selectedPrice = document.getElementById('selected-price');
+var form = document.getElementById('modal-form');
+var closeModalBtn = document.querySelector('.close-btn');
+
+var hiddenPlanField = document.getElementById('hidden-plan');
+var hiddenPriceField = document.getElementById('hidden-price');
+
+let isSubmitting = false;
+
+buttons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    selectedPlan.textContent = button.getAttribute('data-plan');
+	selectedPrice.textContent = button.getAttribute('data-price');
+	hiddenPlanField.value = button.getAttribute('data-plan');
+    hiddenPriceField.value = button.getAttribute('data-price');
+    modal.style.display = 'flex'; // Показываем модальное окно
+	document.body.classList.add('no-scroll');
+  });
+});
+
+// Закрытие модального окна
+closeModalBtn.addEventListener('click', closeModal);
+modal.addEventListener('click', function (event) {
+  if (event.target === modal) closeModal();
+  
+});
+
+function closeModal() {
+  modal.style.display = 'none';
+  clearForm();
+  document.body.classList.remove('no-scroll');
+}
+
+// Очистка формы
+function clearForm() {
+  form.reset(); 
+  hiddenPlanField.value = '';
+  hiddenPriceField.value = '';
+}
+
+// Автоматическое изменение высоты текстового поля
+var textarea = document.getElementById('message');
+textarea.addEventListener('input', function () {
+  this.style.height = 'auto'; 
+  this.style.height = this.scrollHeight + 'px'; 
+});
+
+// Очистка формы после успешной отправки
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); // Предотвращаем стандартное поведение
+	
+  if (isSubmitting) return;
+  isSubmitting = true;
+	
+  const formData = new FormData(form);
+
+  fetch(form.action, {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok || response.redirected) {
+        alert('Форма успешно отправлена!');
+        closeModal(); // Закрываем модальное окно
+        form.reset(); // Очищаем форму
+        window.location.href = 'Thank.html';
+      } else {
+        alert('Произошла ошибка. Попробуйте еще раз.');
+      }
+    })
+    .catch((error) => {
+      console.error('Ошибка сети:', error);
+      alert('Ошибка сети. Проверьте подключение.');
+    });
+});
+
+
+
+
+//Авторский надзор и менеджмент
+document.querySelectorAll('.card-wrapper').forEach(card => {
+        card.addEventListener('click', () => {
+            const textWrapper = card.querySelector('.text-wrapper');
+            textWrapper.classList.toggle('active');
+
+            // Регулируем высоту
+            if (textWrapper.classList.contains('active')) {
+                textWrapper.style.maxHeight = textWrapper.scrollHeight + 'px';
+            } else {
+                textWrapper.style.maxHeight = '0';
+            }
+        });
+    });
+
+document.getElementById('scrollButton').addEventListener('click', function() {
+    const targetBlock = document.getElementById('targetBlock');
+    targetBlock.scrollIntoView({ behavior: 'smooth' });
+  });
+
+// Анимация в конце, зображения для каждой области
+const imagesByArea = {
+  A: ["Image/24.png", "Image/25.png", "Image/27.png", "Image/28.png", "Image/29.png", "Image/30.png", "Image/31.png", "Image/32.png", "Image/33.png"],
+  B: ["Image/21.png", "Image/41.png", "Image/43.png", "Image/10.png", "Image/14.png", "Image/16.png"],
+  C: ["Image/17.png", "Image/18.png", "Image/19.png", "Image/20.png", "Image/21.png", "Image/22.png"],
+  D: ["Image/35.png", "Image/36.png", "Image/37.png", "Image/38.png", "Image/39.png", "Image/42.png", "Image/40.png"],
+  E: ["Image/9.png", "Image/10.png", "Image/11.png"],
+  F: ["Image/12.png", "Image/11.png", "Image/13.png"],
+  G: ["Image/14.png", "Image/15.png", "Image/16.png"],};
+
+const galleryItems = document.querySelectorAll(".gallery img");
+
+// Функция для смены изображения в области
+function changeImage(areaClass) {
+  const areaImages = imagesByArea[areaClass]; 
+  const randomIndex = Math.floor(Math.random() * areaImages.length);
+  const newImageSrc = areaImages[randomIndex];
+
+  const imgElement = document.querySelector(`.${areaClass}`);
+
+  // Избегаем замены на то же изображение
+  if (imgElement.src.includes(newImageSrc)) {
+    return changeImage(areaClass);
+  }
+
+  imgElement.classList.add("hidden");
+  setTimeout(() => {
+    imgElement.src = newImageSrc;
+    imgElement.classList.remove("hidden");
+  }, 500);
+}
+
+// Функция для рандомного выбора области
+function changeRandomArea() {
+  const areas = Object.keys(imagesByArea); 
+  const randomArea = areas[Math.floor(Math.random() * areas.length)];
+  changeImage(randomArea);
+}
+
+setInterval(changeRandomArea, 3500);
+
+
+
---
+++ js/script2scroll.js
@@ -0,0 +1,153 @@
+var container = document.getElementById("track");
+    var imagePath = "Image/"; 
+    var totalImages = 8; 
+
+    // Функция загрузки изображений
+    function loadImages() {
+      // Оригинальные изображения
+      for (var i = 1; i <= totalImages; i++) {
+        var img = document.createElement("img");
+        img.src = imagePath + i + ".png";
+        img.alt = "Image " + i;
+        img.className = "image"; 
+        img.dataset.imageSrc = img.src; 
+        container.appendChild(img);
+      }
+
+      // Дублируем изображения для бесшовного скролла
+      for (var j = 1; j <= totalImages; j++) {
+        var duplicateImg = document.createElement("img");
+        duplicateImg.src = imagePath + j + ".png";
+        duplicateImg.className = "image"; 
+        duplicateImg.dataset.imageSrc = duplicateImg.src;  
+        container.appendChild(duplicateImg);
+      }
+    }
+
+	loadImages();
+
+// Бесконечный скролл
+    container.addEventListener("scroll", function () {
+      var maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+      if (container.scrollLeft <= 0) {
+        // Перемещаемся в конец
+        container.scrollLeft = maxScrollLeft - 1;
+      } else if (container.scrollLeft >= maxScrollLeft) {
+        // Перемещаемся в начало
+        container.scrollLeft = 1;
+      }
+    });
+
+    // Управление колесиком мыши
+    container.addEventListener("wheel", function (e) {
+      e.preventDefault();
+      container.scrollLeft += e.deltaY > 0 ? 100 : -100; 
+    });
+
+
+// Открытие модального окна
+var openModalButtons = document.querySelectorAll('.image-modal-trigger');
+var modal_pict = document.getElementById('popup-modal');
+var modalImage = document.getElementById('popup-image');
+var closeModalButton_2 = document.querySelector('.close-popup');
+
+var imageContainer2 = document.getElementById('popup-image-container');
+var leftArrow = document.querySelector('.left-popup-arrow');
+var rightArrow = document.querySelector('.right-popup-arrow');
+
+var imageSets = {
+    1: [101, 102, 103, 104, 105, 106],
+    2: [201, 202, 203, 204, 205, 206, 207, 208],
+    3: [301, 302, 303, 304, 305, 306],
+    4: [401, 402, 403,404, 405, 406, 407, 408],
+    5: [501, 502, 503, 504, 505, 506, 507, 508],
+    6: [601, 602, 603, 604, 605, 606, 607],
+    7: [701, 702, 703, 704, 705, 706, 707],
+    8: [801, 802, 803, 804, 805, 806]
+};
+
+// Функция открытия модального окна
+
+document.querySelectorAll('.image-modal-trigger').forEach(image => {
+    image.addEventListener('click', function() {
+        const imageIndex = this.dataset.image;  
+        
+        // Для слайдера: генерируем картинки для слайдера
+        loadSliderImages(imageIndex); 
+        // Открываем модальное окно
+        document.getElementById('popup-modal').style.display = 'flex';
+		document.body.classList.add('popup_modal-open');
+		setTimeout(() => {
+            const container_scr = document.getElementById('popup-image-container');
+            const centerPosition = container_scr.scrollWidth / 2 - container.clientWidth / 2;
+            container_scr.scrollLeft = centerPosition;
+        }, 28); 
+		
+    });
+});
+// Функция для загрузки изображений в слайдер
+function loadSliderImages(imageIndex) {
+    imageContainer2.innerHTML = ''; 
+
+    // Пример слайдера для каждого набора изображений
+    const totalImages = imageSets[imageIndex].length;  
+    for (let i = 0; i < totalImages; i++) {
+        const img = document.createElement('img');
+        img.src = `Image/${imageIndex * 100 + (i + 1)}.jpg`;  
+        img.className = 'popup-slider-image';
+        imageContainer2.appendChild(img);
+    }
+}
+
+// Закрытие модального окна
+closeModalButton_2.addEventListener('click', function() {
+    modal_pict.style.display = 'none';
+	document.body.classList.remove('popup_modal-open');
+});
+
+// Закрытие модального окна при клике вне его
+window.addEventListener('click', function(event) {
+    if (event.target === modal_pict) {
+        modal_pict.style.display = 'none';
+		;
+		document.body.classList.remove('popup_modal-open')
+    }
+});
+
+leftArrow.addEventListener('click', () => {
+    imageContainer2.scrollBy({ left: -400, behavior: 'smooth' });
+});
+
+rightArrow.addEventListener('click', () => {
+    imageContainer2.scrollBy({ left: 400, behavior: 'smooth' });
+});
+    
+let isMouseDown = false;
+let startX;
+let scrollLeft;
+    
+function startScrolling(e) {
+	console.log("Начало прокрутки");
+    isMouseDown = true;
+    startX = e.pageX || e.touches[0].pageX;  
+    scrollLeft = imageContainer2.scrollLeft; 
+}
+
+function doScrolling(e) {
+    if (!isMouseDown) return; 
+    const x = e.pageX || e.touches[0].pageX; 
+    const walk = (x - startX); 
+    imageContainer2.scrollLeft = scrollLeft - walk; 
+	console.log("Прокрутка: ", walk, "scrollLeft:", container.scrollLeft);
+}
+
+function stopScrolling() {
+	console.log("Завершение прокрутки");
+    isMouseDown = false;
+}
+
+imageContainer2.addEventListener('mousedown', startScrolling); 
+imageContainer2.addEventListener('mousemove', doScrolling);
+imageContainer2.addEventListener('mouseup', stopScrolling); 
+imageContainer2.addEventListener('mouseleave', stopScrolling); 
